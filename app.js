d23c28417173ad61c3f8ddc71a62d0e3be01e962
Ruslan Ten <ra.ten@stalcom.com>
2020-01-29 16:58:53 +0300

    Fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 firebase.initializeApp({
-    messagingSenderId: '748158835814'
+    messagingSenderId: '448358493027'
 });
 
 
@@ -183,7 +183,7 @@ function getToken() {
 
 
 function sendNotification(notification) {
-    var key = 'BGhVlZcepuYegPgmoF_MGjotKpd2emuAiLwXtBo_mjM7-b5Y-VKvBqoYGODw7JZXFaRe97POKLpY080ciWBxmDE';
+    var key = 'AAAAaGQ_q2M:APA91bGCEOduj8HM6gP24w2LEnesqM2zkL_qx2PJUSBjjeGSdJhCrDoJf_WbT7wpQZrynHlESAoZ1VHX9Nro6W_tqpJ3Aw-A292SVe_4Ho7tJQCQxSezDCoJsnqXjoaouMYIwr34vZTs';
 
     console.log('Send notification', notification);
 
